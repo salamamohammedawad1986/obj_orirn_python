5ac0274e6fe9be9b7297c53291ab0736f265c13c
salamamohammedawad1986 <sohighsalama@gmail.com>
2023-10-20 00:14:28 +0200

    odd and even

--- new_file.py
+++ new_file.py
@@ -5,4 +5,11 @@ import numpy as np
 num = np.arange(5,52,3)
 print('Numbers is :  ',num)
 
-
+# How to find odd and even number:
+for i in num:
+    if(i %2) == 0:
+        print('Even No: ', i)
+    else:
+        print('Odd No: ',i)    
+        
+        
